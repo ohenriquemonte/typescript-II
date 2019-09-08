ecf83752471119e04c5ee3228aef4c237a1bf2b4
Henrique Montenegro <montenegrohenrique@MonteBook.local>
2019-09-08 13:10:44 -0300

    Aula 01.1 - Organização do código através de namespaces

--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,18 +1,19 @@
-abstract class View<T> {
+namespace Views {
+    export abstract class View<T> {
 
-    protected _elemento: JQuery;
+        protected _elemento: JQuery;
 
-    constructor(seletor: string) {
+        constructor(seletor: string) {
 
-        this._elemento = $(seletor);
+            this._elemento = $(seletor);
+        }
+
+        update(model: T) {
+
+            this._elemento.html(this.template(model));
+        }
+
+        abstract template(model: T): string;
+
     }
-
-    update(model: T) {
-
-        this._elemento.html(this.template(model));
-    }
-
-    abstract template(model: T): string;
-
 }
-
