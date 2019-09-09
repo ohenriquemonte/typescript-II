b82e12de7fedc7f2d4af3f31727a3b982fad88b4
Henrique Montenegro <montenegrohenrique@MonteBook.local>
2019-09-09 01:27:32 -0300

    Aula 01.1 - Propriedades readonly

--- app/ts/models/Negociacao.ts
+++ app/ts/models/Negociacao.ts
@@ -1,24 +1,14 @@
 export class Negociacao {
-    
-    constructor(private _data: Date, private _quantidade: number, private _valor: number) {}
 
-    get data() {
+    constructor(readonly data: Date,
+        readonly quantidade: number,
+        readonly valor: number) {
 
-        return this._data;
     }
 
-    get quantidade() {
-
-        return this._quantidade;
-    }
-
-    get valor() {
-
-        return this._valor;
-    }
 
     get volume() {
 
-        return this._quantidade * this._valor;
+        return this.quantidade * this.valor;
     }
 }
